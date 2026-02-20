80b5567f8d3c7206f826019ed040b00e2d9e7ba1
Isthatok74 <terencecyma@gmail.com>
2026-02-20 17:53:59 -0500

    update icon links

--- src/ContentIconPanel.tsx
+++ src/ContentIconPanel.tsx
@@ -12,13 +12,13 @@ function ContentIconPanel(props: iconLinksProps): JSX.Element
 {
     return ( 
       <IconList>
-        <IconListItem><ImageLink height={props.height} hover="Click to join the Discord community!" image={Icons.iconDiscord} link="https://discord.gg/v3dGFCHqnp"/></IconListItem>
-        <IconListItem><ImageLink height={props.height} hover="Click to follow Youtube link"  image={Icons.iconYoutube} link="https://www.youtube.com/@PaperVolleyball"/></IconListItem>
-        <IconListItem><ImageLink height={props.height} hover="Instagram page coming soon!" image={Icons.iconInstagram} link=""/></IconListItem>
+        <IconListItem><ImageLink height={props.height} hover="Join the Discord community!" image={Icons.iconDiscord} link="https://discord.gg/v3dGFCHqnp"/></IconListItem>
+        <IconListItem><ImageLink height={props.height} hover="Subscribe on Youtube!"  image={Icons.iconYoutube} link="https://www.youtube.com/@PaperVolleyball"/></IconListItem>
+        <IconListItem><ImageLink height={props.height} hover="Follow us on Instagram!" image={Icons.iconInstagram} link="https://www.instagram.com/papervolleyball/"/></IconListItem>
         <IconListItem><ImageLink height={props.height} hover="Steam page coming soon!" image={Icons.iconSteam} link=""/></IconListItem>
         <IconListItem><ImageLink height={props.height} hover="Google Play Store page coming soon!" image={Icons.iconPlay} link=""/></IconListItem>
         <IconListItem><ImageLink height={props.height} hover="Apple Store page coming soon!" image={Icons.iconApple} link=""/></IconListItem>
-        <IconListItem><ImageLink height={props.height} hover="Click to send an email" image={Icons.iconEmail} link="mailto:papervolleyballdev@gmail.com"/></IconListItem>
+        <IconListItem><ImageLink height={props.height} hover="Send an email" image={Icons.iconEmail} link="mailto:papervolleyballdev@gmail.com"/></IconListItem>
       </IconList>
     );
 }
